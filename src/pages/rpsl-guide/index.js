43cfaec3bad6d9580ee1d1837c5bce8537a38e68
Carl Yao Agbenyega <aycarl@merit.edu>
2023-08-17 21:34:08 +0000

    update style, reorganize rpsl pages

--- src/pages/rpsl-guide/index.js
+++ src/pages/rpsl-guide/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 
 const RpslGuidePage = () => {
   return (
